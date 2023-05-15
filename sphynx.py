dbeb01a4f51828f09d9de12f45519a289418110b
HavenSelph <havenselph@gmail.com>
2023-05-15 01:08:36 -0500

    added -v --verbose flag to compiler front-end

--- sphynx.py
+++ sphynx.py
@@ -1,21 +1,26 @@
 import argparse
 import pathlib
+from rich import print
 from time import perf_counter
-from compiler import __version__, __author__, __license__
+
+import compiler.lang.common.error
+from compiler import __version__, __author__, __license__  # noqa
 from compiler.lang import lexer as _lexer, parser as _parser
 
 argparser = argparse.ArgumentParser(description="Compiler for Sphynx-Language (.spx)")
 argparser.add_argument("file", type=str, help="The file to compile")
 argparser.add_argument("-o", "--output", type=str, help="The output file")
+argparser.add_argument("-v", "--verbose", action="store_true", help="Prints extra information during compilation")
 args = argparser.parse_args()
 
 file = pathlib.Path(args.file)
 if not file.exists():
-    print(f"File {file} does not exist")
-    exit(1)
+    raise FileNotFoundError(f"File {file} does not exist")
 if not args.output:
     args.output = file.with_suffix(".c")
 output = pathlib.Path(args.output)
+if not output.cwd().exists():
+    raise FileNotFoundError(f"Directory {output.cwd()} does not exist")
 
 print(f"Sphynx Compiler v{__version__} by {__author__} ({__license__})")
 print(f"Compiling {file} to {output}")
@@ -24,9 +29,18 @@ start = perf_counter()
 with open(file, "r") as f:
     source = f.read()
 
-lexer = _lexer.Lexer(str(file), source)
-tokens = lexer.lex()
-parser = _parser.Parser(str(file), tokens)
-ast = parser.parse()
-
+try:
+    lexer = _lexer.Lexer(str(file), source)
+    tokens = lexer.lex()
+    if args.verbose:
+        print(f"Lexed in {perf_counter() - start:.4f}s")
+        print(tokens)
+    parser = _parser.Parser(str(file), tokens)
+    ast = parser.parse()
+except compiler.lang.common.error.SphynxError as e:
+    e.print_error()
+    raise
+if args.verbose:
+    print(f"Parsed in {perf_counter() - start:.4f}s")
+    print(ast)
 print(f"Finished in {perf_counter() - start:.4f}s")
